9b44dedfc4aa2c82f9056e44ab7ce3735ad65711
LászlóFrank <kriszfrank@gmail.com>
2024-10-13 21:49:58 +0200

    Created

---
+++ src/main/java/com/lunchapp/lunchgrouprandomizer/LunchGroupRandomizerApplication.java
@@ -0,0 +1,13 @@
+package com.lunchapp.lunchgrouprandomizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchGroupRandomizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchGroupRandomizerApplication.class, args);
+    }
+
+}
